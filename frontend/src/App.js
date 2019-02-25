cffb9d512262eec7089b546f009cabbea34d4d1b
Fabian Kresler <fabyyy@Fabians-MBP.fritz.box>
2019-02-25 23:33:56 +0100

    Introducing header component Introducing react-router navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,63 @@
-import React, { Component } from 'react';
+import React, {Component} from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink as Link
+} from "react-router-dom";
+import styled from "styled-components";
 
+import ProductIntroduction from "./components/startpage/ProductIntroduction";
+import NotFoundContent from "./components/errorpage/NotFoundContent";
 import ChampionListsApp from "./components/championlist/ChampionListsApp";
 
-class App extends Component {
-  render() {
-    return (
-        <ChampionListsApp/>
-    );
-  }
-}
+const StyledNavigationBar = styled.div`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    padding: 1rem 2rem;
+    display: flex;
+    justify-content: flex-start;
+    box-sizing: border-box;
+    background-color: green;
+    color: #fff;
+    font-weight: bold;
+
+    a:first-child {
+        margin-right: auto;
+    }
+
+    a {
+        padding: 0 0.5rem;
+    }
+`;
+
+const StyledContentWrapper = styled.div`
+    display: block;
+    width: 100%;
+    height: 100%;
+    margin-top: 3rem;
+    padding: 1rem;
+    box-sizing: border-box;
+`;
+
+const App = () => (
+    <Router>
+        <div>
+            <StyledNavigationBar>
+                <Link to="/">Home</Link>
+                <Link to="/mylists">My Lists</Link>
+                <Link to="/error">Error</Link>
+            </StyledNavigationBar>
+            <StyledContentWrapper>
+                <Switch>
+                    <Route exact path="/" component={ProductIntroduction} />
+                    <Route exact path="/mylists" component={ChampionListsApp} />
+                    <Route component={NotFoundContent} />
+                </Switch>
+            </StyledContentWrapper>
+        </div>
+    </Router>
+);
 
 export default App;
---
+++ frontend/src/components/startpage/ProductIntroduction.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+class ProductIntroduction extends Component {
+    render() {
+        return (
+            <div>
+                This is some introductional content!
+            </div>
+        );
+    }
+}
+
+export default ProductIntroduction;
