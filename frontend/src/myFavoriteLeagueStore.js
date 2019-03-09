c05ebc6cba605c273e7e96883447eb23460cd84c
Fabian Kresler <fabyyy@Fabians-MBP.fritz.box>
2019-03-09 14:12:44 +0100

    Introducing cookie middleware to sync state with browser

--- frontend/src/myFavoriteLeagueStore.js
+++ frontend/src/myFavoriteLeagueStore.js
@@ -1,7 +1,32 @@
 import {applyMiddleware, createStore} from "redux";
 import myfavoriteleaguereduxlogger from "redux-logger";
 import myfavoriteleaguereducer from "./reducers";
+import Cookies from "js-cookie";
 
-const myfavoriteleaguemiddleware = applyMiddleware(myfavoriteleaguereduxlogger);
+const MY_FAVORITE_LEAGUE_COOKIE_NAME = "myfavoriteleagueappstate";
 
-export default createStore(myfavoriteleaguereducer, myfavoriteleaguemiddleware);
+// Read previous state from cookies
+let previousState = Cookies.get(MY_FAVORITE_LEAGUE_COOKIE_NAME);
+previousState = previousState ? JSON.parse(previousState) : {};
+
+// Define cookie middleware
+const synchronizeCookieData = ({getState}) => {
+    return next => action => {
+        const result = next(action);
+        Cookies.set(MY_FAVORITE_LEAGUE_COOKIE_NAME, JSON.stringify(getState()));
+        return result;
+    };
+};
+
+// Combine middleware
+const myfavoriteleaguemiddleware = applyMiddleware(
+    myfavoriteleaguereduxlogger,
+    synchronizeCookieData
+);
+
+// Create store
+export default createStore(
+    myfavoriteleaguereducer,
+    previousState,
+    myfavoriteleaguemiddleware
+);
