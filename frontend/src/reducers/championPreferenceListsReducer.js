8748cdb66c1131353492c3a17b185227a7967054
Fabian Kresler <fabyyy@Fabians-MBP.fritz.box>
2019-03-20 21:32:07 +0100

    ChampionPreferenceLists + Beginning reducers

--- frontend/src/reducers/championPreferenceListsReducer.js
+++ frontend/src/reducers/championPreferenceListsReducer.js
@@ -1,18 +1,37 @@
-const championPreferenceListReducer = (
-    state = {
-        userChampionPreferenceLists: {
-            Top: {},
-            Jungle: {},
-            Mid: {},
-            Bottom: {},
-            Support: {}
-        }
-    },
-    action
-) => {
+const initialState = {
+    userChampionPreferenceLists: {
+        Moodboard: {},
+        Top: {},
+        Jungle: {},
+        Mid: {},
+        Bottom: {},
+        Support: {}
+    }
+};
+
+const championPreferenceListReducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_CHAMPION_TO_LIST":
-            break;
+            let currentListId = action.payload.listId;
+            let currentChampionId = action.payload.championId;
+            if (
+                !!state.userChampionPreferenceLists.currentListId ||
+                !!state.userChampionPreferenceLists.currentListId.championId
+            ) {
+                break;
+            }
+            let allListsCopy = {...state.userChampionPreferenceLists};
+            let currentListCopy = {
+                ...allListsCopy.currentListId
+            };
+            currentListCopy[currentChampionId] = {
+                priority: action.payload.priority
+            };
+            allListsCopy.currentListId = currentListCopy;
+            return {
+                ...state,
+                userChampionPreferenceLists: allListsCopy
+            };
         case "SET_CHAMPION_PRIORITY":
             break;
         case "SET_CHAMPION_NOTE":
