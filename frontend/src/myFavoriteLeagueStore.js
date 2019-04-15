5b1c660b839957381d7d4b6c82611c70d9c137dd
Fabian Kresler <fabyyy@Fabians-MBP.fritz.box>
2019-04-15 08:06:38 +0200

    Beginning implementing user data into the central store

--- frontend/src/myFavoriteLeagueStore.js
+++ frontend/src/myFavoriteLeagueStore.js
@@ -6,17 +6,23 @@ import Cookies from "js-cookie";
 
 const MY_FAVORITE_LEAGUE_COOKIE_NAME = "myfavoriteleagueappstate";
 
-// Read previous state from cookies
-let previousState = Cookies.get(MY_FAVORITE_LEAGUE_COOKIE_NAME);
-previousState = previousState ? JSON.parse(previousState) : {};
+// Read previous user data state from cookies
+let previousUserDataState = Cookies.get(MY_FAVORITE_LEAGUE_COOKIE_NAME);
+previousUserDataState = previousUserDataState
+    ? JSON.parse(previousUserDataState)
+    : {};
+// If there was a user id try to recreate the previous state from the backend information
 
 // Define cookie middleware
 const synchronizeCookieData = ({getState}) => {
     return next => action => {
         const result = next(action);
         const currentState = getState();
-        const jsonCurrentState = JSON.stringify(currentState);
-        Cookies.set(MY_FAVORITE_LEAGUE_COOKIE_NAME, JSON.stringify(getState()));
+        const userDataState = currentState ? currentState.userDataState : {};
+        Cookies.set(
+            MY_FAVORITE_LEAGUE_COOKIE_NAME,
+            JSON.stringify(userDataState)
+        );
         return result;
     };
 };
