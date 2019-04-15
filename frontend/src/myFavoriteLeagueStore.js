67973b01f39914add20a17607c7fcc6ddba6ce9a
Fabian Kresler <fabyyy@Fabians-MBP.fritz.box>
2019-04-15 21:29:54 +0200

    How do I implement the GET and POST user data?

--- frontend/src/myFavoriteLeagueStore.js
+++ frontend/src/myFavoriteLeagueStore.js
@@ -2,36 +2,14 @@ import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
 import myfavoriteleaguereduxlogger from "redux-logger";
 import myfavoriteleaguereducer from "./reducers";
-import Cookies from "js-cookie";
+import {getPreviousStateData} from "./utils/stateUtils";
 
-const MY_FAVORITE_LEAGUE_COOKIE_NAME = "myfavoriteleagueappstate";
-
-// Read previous user data state from cookies
-let previousUserDataState = Cookies.get(MY_FAVORITE_LEAGUE_COOKIE_NAME);
-previousUserDataState = previousUserDataState
-    ? JSON.parse(previousUserDataState)
-    : {};
-// If there was a user id try to recreate the previous state from the backend information
-
-// Define cookie middleware
-const synchronizeCookieData = ({getState}) => {
-    return next => action => {
-        const result = next(action);
-        const currentState = getState();
-        const userDataState = currentState ? currentState.userDataState : {};
-        Cookies.set(
-            MY_FAVORITE_LEAGUE_COOKIE_NAME,
-            JSON.stringify(userDataState)
-        );
-        return result;
-    };
-};
+let previousState = await getPreviousStateData();
 
 // Combine middleware
 const myfavoriteleaguemiddleware = applyMiddleware(
     thunk,
-    myfavoriteleaguereduxlogger,
-    synchronizeCookieData
+    myfavoriteleaguereduxlogger
 );
 
 // Create store
