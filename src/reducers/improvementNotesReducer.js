bbf63b401b4c82e9fa5cde07166e24a2276021a1
Fabian Kresler <fabi.kresler@googlemail.com>
2019-09-08 13:25:40 +0200

    Reducer adjustments

--- src/reducers/improvementNotesReducer.js
+++ src/reducers/improvementNotesReducer.js
@@ -1,5 +1,3 @@
-export const IMPROVEMENT_NOTES_DATA_NAME = 'improvementNotesData';
-
 const initialState = {
   improvementNotes: [],
 };
--- src/reducers/staticChampionDataReducer.js
+++ src/reducers/staticChampionDataReducer.js
@@ -1,5 +1,3 @@
-export const STATIC_CHAMPION_DATA_NAME = 'staticChampionData';
-
 const initialState = {
   isStaticChampionDataCorrect: false,
   staticChampionData: {},
