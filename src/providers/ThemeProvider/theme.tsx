163fbaf446913ba8c308ee4846275271f3f6ec68
Fabian Kresler <fabi.kresler@googlemail.com>
2020-06-18 17:23:10 +0200

    Theme

--- src/providers/ThemeProvider/theme.tsx
+++ src/providers/ThemeProvider/theme.tsx
@@ -2,29 +2,137 @@ import { MyFavoriteLeagueTheme } from 'styled-components';
 
 export const lightTheme: MyFavoriteLeagueTheme = {
   colors: {
-    lightBackgroundColor: '#FFFFFF',
-    darkBackgroundColor: '#00A7E1',
-    colorPositive: '#13CE66',
-    colorNegative: '#FF4949',
-    colorWarning: '#FFC82C',
-    fontColorLight: '#F0F4FA',
-    fontColorDark: '#050C24',
-    borderColorLight: '#F0F4FA',
-    borderColorDark: '#050C24',
+    brand: {
+      main: '#01b6f5',
+      dark: '',
+      light: '',
+    },
+    background: {
+      main: '#FFFFFF',
+      secondary: '#00A7E1',
+      highlight: '',
+      disabled: '#F6F7F9',
+    },
+    text: {
+      main: '#050C24',
+      disabled: '#D7D9E2',
+    },
+    action: {
+      main: '#01b6f5',
+      active: '#F6F7F9',
+      hover: '#F6F7F9',
+      disabled: '#F6F7F9',
+    },
+    border: {
+      main: '#A4A7B5',
+      divider: '#D7D9E2',
+      disabled: 'rgba(82, 95, 127, 0.15)',
+    },
+    success: {
+      main: '#13CE66',
+    },
+    error: {
+      main: '#FF4949',
+    },
+    warning: {
+      main: '#FFC82C',
+    },
+  },
+  shadows: {
+    brand: '0 0 2px #01b6f5',
+    success: '0 0 2px #13CE66',
+    error: '0 0 2px #FF4949',
+    warning: '0 0 2px #FFC82C',
+    main: '0 0 2px 0 #A4A7B5',
+  },
+  borderRadius: {
+    main: '0.3rem',
+  },
+  fontSizes: {
+    headlineSmall: '1rem',
+    headlineMedium: '1.25rem',
+    headlineLarge: '1.5rem',
+    textSmall: '0.75rem',
+    textMedium: '0.875rem',
+    textLarge: '1rem',
+  },
+  margins: {
+    marginSmall: '0.75rem',
+    marginMedium: '1rem',
+    marginLarge: '1.25rem',
+  },
+  paddings: {
+    paddingSmall: '0.25rem',
+    paddingMedium: '0.5rem',
+    paddingLarge: '1rem',
   },
 };
 
 export const darkTheme: MyFavoriteLeagueTheme = {
   colors: {
-    lightBackgroundColor: '#093A3E',
-    darkBackgroundColor: '#001011',
-    colorPositive: '#13CE66',
-    colorNegative: '#FF4949',
-    colorWarning: '#FFC82C',
-    fontColorLight: '#F0F4FA',
-    fontColorDark: '#F0F4FA',
-    borderColorLight: '#F0F4FA',
-    borderColorDark: '#F0F4FA',
+    brand: {
+      main: '#01b6f5',
+      dark: '',
+      light: '',
+    },
+    background: {
+      main: '093A3E',
+      secondary: '#001011',
+      highlight: '',
+      disabled: '#F6F7F9',
+    },
+    text: {
+      main: '#F0F4FA',
+      disabled: '#D7D9E2',
+    },
+    action: {
+      main: '#01b6f5',
+      active: '#F6F7F9',
+      hover: '#F6F7F9',
+      disabled: '#F6F7F9',
+    },
+    border: {
+      main: '#A4A7B5',
+      divider: '#D7D9E2',
+      disabled: 'rgba(82, 95, 127, 0.15)',
+    },
+    success: {
+      main: '#13CE66',
+    },
+    error: {
+      main: '#FF4949',
+    },
+    warning: {
+      main: '#FFC82C',
+    },
+  },
+  shadows: {
+    brand: '0 0 2px #01b6f5',
+    success: '0 0 2px #13CE66',
+    error: '0 0 2px #FF4949',
+    warning: '0 0 2px #FFC82C',
+    main: '0 0 2px 0 #A4A7B5',
+  },
+  borderRadius: {
+    main: '0.3rem',
+  },
+  fontSizes: {
+    headlineSmall: '1rem',
+    headlineMedium: '1.25rem',
+    headlineLarge: '1.5rem',
+    textSmall: '0.75rem',
+    textMedium: '0.875rem',
+    textLarge: '1rem',
+  },
+  margins: {
+    marginSmall: '0.75rem',
+    marginMedium: '1rem',
+    marginLarge: '1.25rem',
+  },
+  paddings: {
+    paddingSmall: '0.25rem',
+    paddingMedium: '0.5rem',
+    paddingLarge: '1rem',
   },
 };
 
--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -3,15 +3,69 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface MyFavoriteLeagueTheme {
     colors: {
-      lightBackgroundColor: string;
-      darkBackgroundColor: string;
-      colorPositive: string;
-      colorNegative: string;
-      colorWarning: string;
-      fontColorLight: string;
-      fontColorDark: string;
-      borderColorLight: string;
-      borderColorDark: string;
+      brand: {
+        main: string;
+        dark: string;
+        light: string;
+      };
+      background: {
+        main: string;
+        secondary: string;
+        highlight: string;
+        disabled: string;
+      };
+      text: {
+        main: string;
+        disabled: string;
+      };
+      action: {
+        main: string;
+        active: string;
+        hover: string;
+        disabled: string;
+      };
+      border: {
+        main: string;
+        divider: string;
+        disabled: string;
+      };
+      success: {
+        main: string;
+      };
+      error: {
+        main: string;
+      };
+      warning: {
+        main: string;
+      };
+    };
+    shadows: {
+      brand: string;
+      success: string;
+      error: string;
+      warning: string;
+      main: string;
+    };
+    borderRadius: {
+      main: string;
+    };
+    fontSizes: {
+      headlineSmall: string;
+      headlineMedium: string;
+      headlineLarge: string;
+      textSmall: string;
+      textMedium: string;
+      textLarge: string;
+    };
+    margins: {
+      marginSmall: string;
+      marginMedium: string;
+      marginLarge: string;
+    };
+    paddings: {
+      paddingSmall: string;
+      paddingMedium: string;
+      paddingLarge: string;
     };
   }
 }
