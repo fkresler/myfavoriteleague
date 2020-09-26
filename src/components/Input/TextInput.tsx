43c69d30e9dad9eee70183b6a745391762340f1a
Fabian Kresler <fabi.kresler@googlemail.com>
2020-09-26 15:03:04 +0200

    TextInput

---
+++ src/components/Input/TextInput.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export interface ITextInput {
+  className?: string;
+  label?: string;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+  value?: string;
+  placeholder?: string;
+  hasError?: boolean;
+  isReadOnly?: boolean;
+  isDisabled?: boolean;
+  isRequired?: boolean;
+  hasAutoFocus?: boolean;
+  onChange?: (e) => void;
+  onKeyPress?: (e) => void;
+}
+
+const StyledInput = styled.input`
+  display: block;
+  background-color: ${({ theme }) => theme.colors.background.main};
+`;
+
+export const TextInput: React.FC<ITextInput> = ({
+  className,
+  label,
+  prefix,
+  suffix,
+  value,
+  placeholder,
+  hasError,
+  isReadOnly,
+  isDisabled,
+  onChange,
+  onKeyPress,
+}) => {};
+
+export default TextInput;
