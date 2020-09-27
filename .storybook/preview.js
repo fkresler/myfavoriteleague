e03a877b1603242632bc82773e2c5c72f71c8a41
Fabian Kresler <fabi.kresler@googlemail.com>
2020-09-27 13:19:31 +0200

    Storybook Theming

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,10 @@
+import { addDecorator } from '@storybook/react';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+import { lightTheme, darkTheme } from '@/theme';
+
+const themes = [lightTheme, darkTheme];
+addDecorator(withThemesProvider(themes));
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
--- src/theme/index.stories.tsx
+++ src/theme/index.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeContext } from 'styled-components';
 
 const DebugColorWrapper = styled.div`
   display: flex;
@@ -35,51 +35,69 @@ const ThemeColor: React.FC<{ text: string; color: string }> = ({ text, color }) 
   );
 };
 
-export const TextColors = () => (
-  <DebugColorWrapper>
-    <ThemeColor text="Light" color={theme.colors.text.light} />
-    <ThemeColor text="Dark" color={theme.colors.text.dark} />
-  </DebugColorWrapper>
-);
+export const TextColors = () => {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <DebugColorWrapper>
+      <ThemeColor text="Light" color={theme.colors.text.light} />
+      <ThemeColor text="Dark" color={theme.colors.text.dark} />
+    </DebugColorWrapper>
+  );
+};
 
-export const BrandColors = () => (
-  <DebugColorWrapper>
-    <ThemeColor text="Main" color={theme.colors.brand.main} />
-    <ThemeColor text="Dark" color={theme.colors.brand.dark} />
-    <ThemeColor text="Light" color={theme.colors.brand.light} />
-  </DebugColorWrapper>
-);
+export const BrandColors = () => {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <DebugColorWrapper>
+      <ThemeColor text="Main" color={theme.colors.brand.main} />
+      <ThemeColor text="Dark" color={theme.colors.brand.dark} />
+      <ThemeColor text="Light" color={theme.colors.brand.light} />
+    </DebugColorWrapper>
+  );
+};
 
-export const BackgroundColors = () => (
-  <DebugColorWrapper>
-    <ThemeColor text="Main" color={theme.colors.background.main} />
-    <ThemeColor text="Secondary" color={theme.colors.background.secondary} />
-    <ThemeColor text="Highlight" color={theme.colors.background.highlight} />
-    <ThemeColor text="Disabled" color={theme.colors.background.disabled} />
-  </DebugColorWrapper>
-);
+export const BackgroundColors = () => {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <DebugColorWrapper>
+      <ThemeColor text="Main" color={theme.colors.background.main} />
+      <ThemeColor text="Secondary" color={theme.colors.background.secondary} />
+      <ThemeColor text="Highlight" color={theme.colors.background.highlight} />
+      <ThemeColor text="Disabled" color={theme.colors.background.disabled} />
+    </DebugColorWrapper>
+  );
+};
 
-export const ActionColors = () => (
-  <DebugColorWrapper>
-    <ThemeColor text="Main" color={theme.colors.action.main} />
-    <ThemeColor text="Active" color={theme.colors.action.active} />
-    <ThemeColor text="Hover" color={theme.colors.action.hover} />
-    <ThemeColor text="Disabled" color={theme.colors.action.disabled} />
-  </DebugColorWrapper>
-);
+export const ActionColors = () => {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <DebugColorWrapper>
+      <ThemeColor text="Main" color={theme.colors.action.main} />
+      <ThemeColor text="Active" color={theme.colors.action.active} />
+      <ThemeColor text="Hover" color={theme.colors.action.hover} />
+      <ThemeColor text="Disabled" color={theme.colors.action.disabled} />
+    </DebugColorWrapper>
+  );
+};
 
-export const BorderColors = () => (
-  <DebugColorWrapper>
-    <ThemeColor text="Main" color={theme.colors.border.main} />
-    <ThemeColor text="Divider" color={theme.colors.border.divider} />
-    <ThemeColor text="Disabled" color={theme.colors.border.disabled} />
-  </DebugColorWrapper>
-);
+export const BorderColors = () => {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <DebugColorWrapper>
+      <ThemeColor text="Main" color={theme.colors.border.main} />
+      <ThemeColor text="Divider" color={theme.colors.border.divider} />
+      <ThemeColor text="Disabled" color={theme.colors.border.disabled} />
+    </DebugColorWrapper>
+  );
+};
 
-export const FeedbackColors = () => (
-  <DebugColorWrapper>
-    <ThemeColor text="colorPositive" color={theme.colors.success.main} />
-    <ThemeColor text="colorNegative" color={theme.colors.error.main} />
-    <ThemeColor text="colorWarning" color={theme.colors.warning.main} />
-  </DebugColorWrapper>
-);
+export const FeedbackColors = () => {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <DebugColorWrapper>
+      <ThemeColor text="colorPositive" color={theme.colors.success.main} />
+      <ThemeColor text="colorNegative" color={theme.colors.error.main} />
+      <ThemeColor text="colorWarning" color={theme.colors.warning.main} />
+    </DebugColorWrapper>
+  );
+};
