19895fca6846f02588a509d916d5a52457824958
Fabian Kresler <fabi.kresler@googlemail.com>
2020-10-25 13:05:52 +0100

    Modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,46 +4,72 @@ import styled from 'styled-components';
 
 export interface IModal {
   isOpen: boolean;
-  size: 'small' | 'medium' | 'huge' | 'full';
-  title?: string | React.ReactNode;
-  footer?: React.ReactNode;
+  title?: string;
   onRequestClose?: () => void;
 }
 
 const ModalBackground = styled.div<{ isOpen: boolean }>`
-  z-index: -1;
+  position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  opacity: 0;
-  background: ${({ theme }) => theme.colors.greyDark.default};
-  position: fixed;
-  transition: opacity 0.3s, z-index 0.3s;
-  ${(isOpen) =>
-    isOpen &&
-    `
-            visibility: visible;
-            z-index: 1000000;
-            opacity: 0.5;
-        `};
+  background-color: ${({ theme }) => theme.colors.greyDark.default};
+  z-index: 1000000;
+  opacity: 0.5;
 `;
 
-export const Modal: React.FC<IModal> = ({
-  isOpen,
-  size,
-  title,
-  footer,
-  onRequestClose,
-  children,
-}) => {
+const ModalboxContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1000001;
+  width: 40rem;
+  max-width: 100%;
+  height: 25rem;
+  max-height: 100%;
+  background-color: ${({ theme }) => theme.colors.base.default};
+  color: ${({ theme }) => theme.colors.base.text};
+  box-shadow: ${({ theme }) => theme.shadows.default};
+  border-radius: 3px;
+`;
+
+const ModalboxHeadlineContainer = styled.div`
+  flex: 0 0 auto;
+  display: block;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
+  text-overflow: ellipsis;
+  font-size: 150%;
+  line-height: 150%;
+  padding: 1rem;
+  border-bottom: 1px solid ${({ theme }) => theme.borders.default};
+`;
+
+const ModalboxContentContainer = styled.div`
+  flex: 1 1 100%;
+  display: block;
+  overflow: auto;
+  padding: 1rem;
+  overflow: auto;
+`;
+
+export const Modal: React.FC<IModal> = ({ isOpen, title, onRequestClose, children }) => {
   if (isOpen) {
     return createPortal(
-      <ModalBackground isOpen={isOpen} onClick={() => onRequestClose && onRequestClose()}>
-        <div>This will be Modal Content</div>
-      </ModalBackground>,
+      <>
+        <ModalBackground isOpen={isOpen} onClick={() => onRequestClose && onRequestClose()} />
+        <ModalboxContainer>
+          {title && <ModalboxHeadlineContainer>{title}</ModalboxHeadlineContainer>}
+          <ModalboxContentContainer>{children}</ModalboxContentContainer>
+        </ModalboxContainer>
+      </>,
       document.body,
     );
   }
---
+++ src/mocks/StaticContent.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+export const StaticLoremIpsum: React.ReactNode = (
+  <div>
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu accumsan nibh. Etiam at
+      sollicitudin dolor. Integer rhoncus ligula at ullamcorper porta. Curabitur congue eget elit
+      eleifend fringilla. Suspendisse commodo pretium lacus, at auctor tellus maximus sit amet.
+      Aenean ante magna, rutrum vel risus in, lobortis pulvinar justo. Donec tristique, enim
+      molestie dignissim tincidunt, augue neque dignissim erat, sed commodo dui enim at erat.
+    </div>
+    <div>
+      In accumsan nulla pharetra sem elementum laoreet. In vehicula sapien a lacus hendrerit
+      tristique. Maecenas non nulla viverra, varius lacus vel, pellentesque purus. Etiam vitae felis
+      turpis. Nulla ut lacinia libero. Pellentesque vel consequat risus, vitae euismod lectus. Donec
+      blandit lacus consequat aliquam tempor.
+    </div>
+    <div>
+      Aliquam sapien dolor, pharetra vel risus at, rhoncus maximus dolor. Curabitur eu euismod mi.
+      Ut urna enim, luctus vel nibh elementum, tristique viverra odio. Duis augue dui, maximus et
+      sem vitae, vulputate eleifend tellus. Donec fringilla sollicitudin dui, eu consequat magna
+      viverra eu. Suspendisse potenti. In egestas, lacus facilisis elementum tempor, nibh massa
+      fringilla erat, ut efficitur purus tellus suscipit dui.
+    </div>
+    <div>
+      Etiam ullamcorper ipsum orci. Vestibulum iaculis purus vitae lacus mollis, eget mattis sapien
+      ultrices. Donec eget nisi augue. Vivamus ultrices tellus massa. Ut molestie diam vestibulum
+      dolor iaculis, vel porttitor justo sagittis. Phasellus sed cursus ipsum. Sed ut nunc mauris.
+      Ut consectetur urna ac semper sollicitudin. In nisl ligula, facilisis a dignissim eget,
+      vehicula quis nisi. Pellentesque feugiat est non accumsan condimentum. Donec imperdiet lectus
+      vel imperdiet maximus. Pellentesque sed sodales elit. Morbi mollis tortor a ante congue
+      efficitur. Cras elementum est nec ipsum tempor cursus. Proin auctor rutrum ante, vel rhoncus
+      ligula venenatis vitae.
+    </div>
+    <div>
+      Etiam eget urna eleifend, maximus ipsum vel, vestibulum risus. Nullam erat leo, auctor eu diam
+      sed, euismod faucibus mauris. Nulla nec odio nisi. Nam eu leo vitae sapien ullamcorper lacinia
+      eu non lorem. Maecenas quis elit sem. Donec sit amet magna in turpis fringilla ornare. Donec
+      convallis nibh arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+      inceptos himenaeos. Aliquam rutrum non neque at auctor. Aliquam suscipit aliquam imperdiet.
+      Curabitur sodales turpis nec dui feugiat, non commodo leo vehicula. Nam tempor consectetur
+      diam, a consequat eros. Nam at neque mollis, dignissim elit ut, efficitur sem. Sed dapibus
+      pellentesque lectus ac iaculis.
+    </div>
+    <div>
+      Duis non euismod nulla, id tempor mi. Sed at dapibus ex. Donec eu aliquet ex. Vivamus interdum
+      luctus dui. Aliquam hendrerit facilisis mi, in venenatis nulla aliquet ut. Donec vitae justo
+      euismod, pharetra augue ut, dapibus nisi. Aliquam vulputate euismod arcu quis tincidunt. Morbi
+      eu arcu sed lectus fringilla lacinia ac in velit. Curabitur nec odio ac odio rutrum tincidunt
+      cursus a elit. Sed quis blandit massa. Suspendisse purus purus, tincidunt a enim eget,
+      accumsan interdum nunc.
+    </div>
+    <div>
+      Cras venenatis placerat risus nec molestie. Nam eget ante convallis, tincidunt massa vel,
+      viverra ex. Nunc sed ipsum neque. Curabitur pharetra libero nec metus suscipit, sit amet
+      malesuada ante venenatis. Morbi bibendum scelerisque eros, quis congue ex volutpat sed.
+      Vestibulum volutpat et ex quis tempus. Sed dictum iaculis felis, et mattis nibh vulputate
+      pretium. Aliquam molestie, sem vitae dapibus laoreet, augue purus posuere eros, in placerat
+      mauris urna id leo. Duis cursus risus quis elementum pulvinar.
+    </div>
+    <div>
+      Nulla eleifend hendrerit gravida. In quis dignissim sapien. Vivamus ac arcu id libero faucibus
+      facilisis. Nulla mattis efficitur neque et interdum. Etiam dapibus lobortis dolor, a dictum
+      turpis venenatis vitae. Cras rutrum est erat, sit amet tincidunt libero porta in. Fusce
+      tincidunt sed risus in commodo. In urna arcu, ultricies ut nulla et, aliquam pulvinar ex. Sed
+      sit amet mauris nec neque porta ultrices suscipit in quam.
+    </div>
+  </div>
+);
+
+export default StaticLoremIpsum;
